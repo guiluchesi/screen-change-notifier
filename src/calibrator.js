259fdf2eb6edeee59ae3e96b4b03a20fb158cec7
Guilherme Luchesi <gui.olhenrique@gmail.com>
2021-12-26 12:56:03 -0300

    feat: move calibrator to a function

--- src/calibrator.js
+++ src/calibrator.js
@@ -2,15 +2,24 @@ const PNG = require('pngjs').PNG
 const fs = require('fs')
 const pixelmatch = require('pixelmatch')
 
-const screen1 = PNG.sync.read(fs.readFileSync('../img1.png'))
-const screen2 = PNG.sync.read(fs.readFileSync('../img2.png'))
+/**
+ * Compare two images and log the amout of pixel changed between the images
+ * @param  {string} imageBasePath - The path to the base image
+ * @param  {string} imageTocComparePath - The path to the image to compare with
+ */
+const calibrator = (imageBasePath, imageTocComparePath) => {
+  const screen1 = PNG.sync.read(fs.readFileSync(imageBasePath))
+  const screen2 = PNG.sync.read(fs.readFileSync(imageTocComparePath))
 
-const pixelChanged = pixelmatch(
-  screen1.data,
-  screen2.data,
-  null,
-  screen1.width,
-  screen1.height
-)
+  const pixelChanged = pixelmatch(
+    screen1.data,
+    screen2.data,
+    null,
+    screen1.width,
+    screen1.height
+  )
 
-console.log('Amount of pixels changed:', pixelChanged)
+  console.log('Amount of pixels changed:', pixelChanged)
+}
+
+calibrator('../img1.png', '../img2.png')
